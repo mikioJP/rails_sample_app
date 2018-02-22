f6ea2688bc6b3f2ffbd18677f1e08694b67d2adf
Mikio Kobayashi <wagnaria441@yahoo.co.jp>
2018-02-22 18:32:45 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,13 @@ class UsersController < ApplicationController
    
    
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   
   
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
     #debugger
   end
   
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     attr_accessor :remember_token, :activation_token 
     before_save :downcase_email
     before_create :create_activation_digest 
+    
     validates :name,presence:true, length:{maximum:50}
      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email,presence:true, length:{maximum:255},
@@ -36,6 +37,15 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  def activate
+     update_columns(activated: true, activated_at: Time.zone.now)
+  end
+  
+  def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+  end
+  
   
   private 
   
