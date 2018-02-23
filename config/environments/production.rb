124c83ba69c8e2530d477e344cda3b3ada366ff6
Mikio Kobayashi <wagnaria441@yahoo.co.jp>
2018-02-23 10:55:34 +0000

    Modify chapter11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
      :authentication => :plain ,
      :user_name => ENV['SENDGRID_USERNAME'],
      :password => ENV['SENDGRID_PASSWORD'],
-     :domain => 'heroku.com',
+     :domain => 'herokuapp.com',
      :enablw_starttls_auto => true
      
    }
