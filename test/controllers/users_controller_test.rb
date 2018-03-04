ab3f45ea75660c6738d52b581e854085108c9e65
Mikio Kobayashi <wagnaria441@yahoo.co.jp>
2018-03-04 15:19:31 +0000

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,16 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  
   def setup
-    @user = users(:michael)
+    @user       = users(:michael)
     @other_user = users(:archer)
   end
-  
-    
-  
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
@@ -19,25 +21,36 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
+
   test "should redirect update when not logged in" do
-    patch user_path(@user), params:{user:{name: @user.name, email: @user.email}}
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
   end
-  
-  test "should redirect index when not logged in " do
-    get users_path
-    assert_redirected_to login_url
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
   
   test "should redirect destroy when not logged in" do
-    assert_no_difference "User.count" do
+    assert_no_difference 'User.count' do
       delete user_path(@user)
     end
     assert_redirected_to login_url
   end
-  
+
   test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
@@ -46,7 +59,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
   
-  
-    
+   test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
 
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 end
